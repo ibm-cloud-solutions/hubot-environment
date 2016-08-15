65c85f2b1df522faa8d3e099473939446c96141b
lanzen@us.ibm.com <lanzen@us.ibm.com>
2016-08-15 09:36:48 -0400

    Replace asterisks with pound signs to hide sensitive values

--- src/scripts/env.js
+++ src/scripts/env.js
@@ -76,7 +76,7 @@ module.exports = robot => {
 						}
 						return sensitive;
 					})) {
-						data = '********************';
+						data = '####################';
 					}
 					envList += key + '=' + data;
 				}
